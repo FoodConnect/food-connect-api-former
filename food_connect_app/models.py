06149c097af0a9ba7c6c7012b6ec8a7c1972bf12
Leah Perri <perrileah2023@gmail.com>
2024-02-19 13:51:00 -0600

    links postgres db in settings.py file and creates models

--- food_connect_app/models.py
+++ food_connect_app/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class UserRole(Enum):
+    DONOR = 'donor'
+    CHARITY = 'charity'
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    business_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=50, choices=[(role.value, role.name) for role in UserRole])
+    EIN_Number = models.CharField(max_length=255)
+    image_data = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Charity(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Donor(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Donation(models.Model):
+    id = models.AutoField(primary_key=True)
+    donor_id = models.ForeignKey(Donor, on_delete=models.CASCADE)
+    image_data = models.CharField(max_length=255)
+    description = models.TextField()
+    inventory = models.IntegerField()
+    pick_up_deadline = models.DateTimeField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    charity_id = models.ForeignKey(Charity, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartedDonation(models.Model):
+    id = models.AutoField(primary_key=True)
+    donation_id = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    donation_id = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    charity_id = models.ForeignKey(Charity, on_delete=models.CASCADE)
+    donation_receipt = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class DonationCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    donation_id = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
