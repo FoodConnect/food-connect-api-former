5525f8071c68125eb59ed92d28cabc620dadaac6
Leah Perri <perrileah2023@gmail.com>
2024-02-23 14:37:31 -0600

    tries to troubleshoot viewsets file

--- food_connect_app/viewsets.py
+++ food_connect_app/viewsets.py
@@ -17,44 +17,44 @@ from .serializers import (
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class CharityViewSet(viewsets.ModelViewSet):
     queryset = Charity.objects.all()
     serializer_class = CharitySerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class DonorViewSet(viewsets.ModelViewSet):
     queryset = Donor.objects.all()
     serializer_class = DonorSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class DonationViewSet(viewsets.ModelViewSet):
     queryset = Donation.objects.all()
     serializer_class = DonationSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class CartedDonationViewSet(viewsets.ModelViewSet):
     queryset = CartedDonation.objects.all()
     serializer_class = CartedDonationSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 class DonationCategoryViewSet(viewsets.ModelViewSet):
     queryset = DonationCategory.objects.all()
     serializer_class = DonationCategorySerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
